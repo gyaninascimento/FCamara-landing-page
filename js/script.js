05ef4320a89ada23f2c33c8c1d124d99a0ff63ef
Gyani Nascimento <gyanipnascimento@gmail.com>
2021-10-10 12:34:06 -0300

    js agendamento

---
+++ js/script.js
@@ -0,0 +1,42 @@
+document.getElementById("Liberar").addEventListener("click", LibrerarMesas);
+document.getElementById("Finalizar").addEventListener("click", FinalizarReserva);
+
+function LibrerarMesas() {
+    var imagens = document.getElementsByTagName("img");
+
+    for (var cont in imagens) {
+        if (imagens[cont].name == "MesaEscolhida") {
+            imagens[cont].src = "img/MesaLivre.png ";
+            imagens[cont].name = "MesaLivre";
+        } 
+    }
+}
+
+function VerificarMesas (name, id){
+    if (name == "MesaLivre"){
+        OcuparMesas(id);
+    }else{
+        (name == "MesaOcupada"); {
+            alert("A mesa está Ocupada!");
+    }
+}
+}
+
+$("#MesaIndisponivel").addClass("MesaIndisponivel");
+
+function OcuparMesas(id) {
+    document.getElementById(id).src = "img/MesaEscolhida.png ";
+    document.getElementById(id).name = "MesaEscolhida";
+}
+
+function FinalizarReserva() {
+    debugger;
+    var imagens = document.getElementsByTagName("img");
+
+    for (var cont in imagens) {
+        if (imagens[cont].name == "MesaEscolhida") {
+            imagens[cont].src = "img/MesaOcupada.png";
+            imagens[cont].name = "MesaOcupada";
+        } 
+    }
+}
